65f59ecc31d19663c8b8adc2e83256f4e5816619
Kavya Srivastava <skavya12669@gamil.com>
2026-06-06 16:16:34 +0530

    Updated

---
+++ linked_list/creating_and_traversing_through_a_linked_list.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node* next;
+    Node(int x){
+        data=x;
+        next=NULL;
+    }
+};
+int main(){
+    // create linked list manually
+    Node* head;
+    Node* first=    new Node(10);
+    Node* second=   new Node(20);
+    Node* third=    new Node(30);
+    Node* fourth=   new Node(40);
+    Node* fifth=    new Node(50);
+    head=first;
+    head -> next= second;
+    head -> next ->next =third;
+    head -> next ->next -> next =fourth;
+    head -> next ->next -> next-> next=fifth;
+
+    // Treverse
+    Node* temp=head;
+    while(temp != NULL){
+        cout<< temp -> data <<" ";
+        temp=temp-> next;
+    }
+
+
+
+
+}
